152a5c103278e9ebb4a0a4f56633962cd6384352
xuweichen <next_100@sina.com>
2012-12-24 17:56:04 +0800

    add UI for SlimJS

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @author: xuweichen
+ * @date: 12-12-12 上午9:59
+ * @descriptions
+ */
+    /*
+var filesPath = $('#J_filesPath').val().trim();
+
+filesPath && (function (){
+    var compressor = require('compressor.js'),
+        fs = require('fs');
+
+    fs.open(filesPath, function(err, fd){
+
+    });
+})();
+*/
+//compressor.engine.setCompiler(config.compilers['google']);
+//compressor.compress('e:\\tfs2010\\Hotel\\Booking\\Branch\\Branch1108_1121\\Present\\WebResource\\Online\\JavaScript\\domestic\\lazyload.js');
+
