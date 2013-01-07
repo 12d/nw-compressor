ba46cda2aa007828d738b8e2590dcb98d4a4572d
xuweichen <next_100@sina.com>
2013-01-07 18:48:45 +0800

    add version number controll

---
+++ core/releaser.js
@@ -0,0 +1,36 @@
+/**
+ * @author: xuweichen
+ * @date: 13-1-7 下午2:46
+ * @descriptions
+ */
+var Util = require('../lib/util/helper.js'),
+    fs = require('fs'),
+    rJsFile = /\.js$/;
+
+function Releaser(options){
+    var DEFAULT_OPTIONS = {
+            releaseDir: ''
+        },
+        self = this;
+
+    self.options = Util.mix(DEFAULT_OPTIONS, options||{});
+
+}
+function copyFile(dir, newDir){
+    fs.readFile(dir, function(err, data){
+        console.log(newDir);
+        fs.writeFile(newDir, data);
+    });
+}
+function addVersion(file, version){
+    return file.replace(rJsFile, '.v'+version+'.js');
+}
+Releaser.prototype = {
+    constructor: Releaser,
+    release: function (file, version){
+        var self = this;
+        copyFile(file, self.options.releaseDir+addVersion(file, version))
+    }
+}
+
+module.exports = Releaser;
--- index.js
+++ index.js
@@ -4,13 +4,16 @@
  * @descriptions
  */
 var Version = require('./core/version.js'),
+    Releaser = require('./core/releaser.js'),
+    Path = require('./lib/util/path.js'),
     DB = require('./core/database.js');
 
-var db = new DB();
+var db = new DB(),
+    releaser = new Releaser();
+
 db.connect('E:\\cxw\\version-ctrl\\s\\j\\data\\version.json', function (){
     vers = new Version(db);
     vers.refresh('/s/j/app/promo.js');
+    releaser.release(Path.realpath('/s/j/app/promo.js'), 8);
     //db.close();
 });
-
-
